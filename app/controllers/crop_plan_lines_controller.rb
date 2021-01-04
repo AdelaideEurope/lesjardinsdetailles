d12769132190b92c170345020e0c106f18c1e9be
Adelaide Blot <adelaide.blot@hotmail.fr>
2021-01-05 00:49:24 +0100

    helpers fix

--- app/controllers/crop_plan_lines_controller.rb
+++ app/controllers/crop_plan_lines_controller.rb
@@ -4,7 +4,7 @@ class CropPlanLinesController < ApplicationController
     authorize @crop_plan_lines
     @gardens = Garden.includes(:beds)
     number_days_since_first_day_of_year = Date.today.strftime("%j").to_i
-    @percentage_number_days_since_first_day_of_year = get_elapsed_days_since_beginning_of_year(number_days_since_first_day_of_year)
+    @percentage_number_days_since_first_day_of_year = helpers.get_elapsed_days_since_beginning_of_year(number_days_since_first_day_of_year)
   end
 end
 
