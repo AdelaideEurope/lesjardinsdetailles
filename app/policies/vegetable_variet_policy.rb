1745159ed1658ac6c4996ae5607c2429406e4c2b
Adelaide Blot <adelaide.blot@hotmail.fr>
2021-01-05 22:12:15 +0100

    product search

---
+++ app/policies/vegetable_variet_policy.rb
@@ -0,0 +1,19 @@
+class VegetableVarietPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+
+  def index?
+    if user.manager
+      true
+    end
+  end
+
+  def show?
+    if user.manager
+      true
+    end
+  end
+end
