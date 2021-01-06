3254b248f32a8d48055dbf1129f4b864d71cccb7
Adelaide Blot <adelaide.blot@hotmail.fr>
2021-01-07 00:03:41 +0100

    mobile layout + drawer product in crop plan and active record optim

--- app/controllers/crop_plan_lines_controller.rb
+++ app/controllers/crop_plan_lines_controller.rb
@@ -2,7 +2,7 @@ class CropPlanLinesController < ApplicationController
   def index
     @crop_plan_lines = CropPlanLine.all
     authorize @crop_plan_lines
-    @gardens = Garden.includes(:beds)
+    @gardens = Garden.includes(:beds => :crop_plan_lines)
     number_days_since_first_day_of_year = Date.today.strftime("%j").to_i
     @percentage_number_days_since_first_day_of_year = helpers.get_elapsed_days_since_beginning_of_year(number_days_since_first_day_of_year)
   end
