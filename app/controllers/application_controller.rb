f2548c3fe352d3b7cf01aa8b3c0e3621bc2907d6
Adelaide Blot <adelaide.blot@hotmail.fr>
2021-01-10 18:35:02 +0100

    redirect to dashboard after signin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,7 +16,7 @@ class ApplicationController < ActionController::Base
 
   def after_sign_in_path_for(resource)
     if resource.worker
-      farm_path(resource.farm_id)
+      dashboard_path(resource.farm_id)
     end
   end
 
