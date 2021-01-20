278ae650b56b07a009bef9635532beaed65cf47e
Adelaide Blot <adelaide.blot@hotmail.fr>
2021-01-21 00:07:23 +0100

    layout + user show page

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,14 +3,11 @@ class ProductsController < ApplicationController
     if params[:query].present?
       sql_query = "name ILIKE :query"
       @products = Product.where(sql_query, query: "%#{params[:query]}%").where(farm_id: current_user.farm_id).order(:slug)
+    elsif params[:sort] != "product_group"
+      @products = Product.includes(:product_group, :photo_attachment).where(farm_id: current_user.farm_id).order(params[:sort])
     else
-      @products = Product.includes(:product_group, :photo_attachment).where(farm_id: current_user.farm_id).order(:slug)
+      @products = Product.includes(:product_group, :photo_attachment).where(farm_id: current_user.farm_id).order("product_groups.name")
     end
     authorize @products
   end
-
-  # def show
-  #   @product = Product.find(params[:id])
-  #   authorize @product
-  # end
 end
