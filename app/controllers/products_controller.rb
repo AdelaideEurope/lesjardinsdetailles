1ac31d67abbdedb57f3954c2987d1535d82fd737
Adelaide Blot <adelaide.blot@hotmail.fr>
2021-02-16 21:54:30 +0100

    photo attachment - product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -36,9 +36,10 @@ class ProductsController < ApplicationController
   end
 
   def create
-    @product = Product.new(name: params[:product][:name].downcase, product_type: params[:product][:product_type], general_unit: params[:product][:general_unit], general_price_final_client: params[:product][:general_price_final_client], general_price_intermediary: params[:product][:general_price_intermediary], yearly_bed_count: params[:product][:yearly_bed_count], yield_per_sqm: params[:product][:yield_per_sqm], estimated_turnover: params[:product][:estimated_turnover], product_group_id: params[:product][:product_group_id], color: params[:product][:color], spacing: params[:product][:spacing], row_count: params[:product][:row_count], loss_percentage: params[:product][:loss_percentage], growth_duration_in_weeks: params[:product][:growth_duration_in_weeks], harvest_duration_in_weeks: params[:product][:harvest_duration_in_weeks], picture: params[:product][:picture], farm_id: params[:farm_id])
+    @product = Product.new(name: params[:product][:name].downcase, product_type: params[:product][:product_type], general_unit: params[:product][:general_unit], general_price_final_client: params[:product][:general_price_final_client], general_price_intermediary: params[:product][:general_price_intermediary], yearly_bed_count: params[:product][:yearly_bed_count], yield_per_sqm: params[:product][:yield_per_sqm], estimated_turnover: params[:product][:estimated_turnover], product_group_id: params[:product][:product_group_id], color: params[:product][:color], spacing: params[:product][:spacing], row_count: params[:product][:row_count], loss_percentage: params[:product][:loss_percentage], growth_duration_in_weeks: params[:product][:growth_duration_in_weeks], harvest_duration_in_weeks: params[:product][:harvest_duration_in_weeks], farm_id: params[:farm_id])
     authorize @product
     @product.save
+    @product.photo.attach(params[:product][:picture]) if params[:product][:picture]
     @fertilization_need = FertilizationNeed.new(fertilization_type: params[:ferti][:type], unit: params[:ferti][:unit], quantity: params[:ferti][:quantity], product_id: @product&.id)
       if @fertilization_need.save
       flash[:notice] = "Produit créé avec succès !"
@@ -53,8 +54,10 @@ class ProductsController < ApplicationController
   def update
     authorize @product
     convert_euro_params_to_cents(@product)
-
     if @product.update(product_params)
+      if params[:product][:picture]
+        @product.photo.attach(params[:product][:picture])
+      end
       if @product.has_ferti?
         @fertilization_need = @product.fertilization_need
         @fertilization_need.update(fertilization_type: params[:ferti][:type], unit: params[:ferti][:unit], quantity: params[:ferti][:quantity], product_id: @product.id)
@@ -107,7 +110,7 @@ class ProductsController < ApplicationController
 
 
   def product_params
-    params.require(:product).permit(:name, :product_type, :general_unit, :general_price_final_client, :general_price_intermediary, :yearly_bed_count, :yield_per_sqm, :estimated_turnover, :product_group_id, :color, :spacing, :row_count, :loss_percentage, :growth_duration_in_weeks, :harvest_duration_in_weeks, :picture)
+    params.require(:product).permit(:name, :product_type, :general_unit, :general_price_final_client, :general_price_intermediary, :yearly_bed_count, :yield_per_sqm, :estimated_turnover, :product_group_id, :color, :spacing, :row_count, :loss_percentage, :growth_duration_in_weeks, :harvest_duration_in_weeks)
   end
 
   def convert_euro_params_to_cents(product)
