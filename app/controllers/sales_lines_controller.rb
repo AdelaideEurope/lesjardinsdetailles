6a3cf91e402263c1e88dca68c8db27707995f86f
Adelaide Blot <adelaide.blot@hotmail.fr>
2021-03-14 20:45:39 +0100

    fix add lines

--- app/controllers/sales_lines_controller.rb
+++ app/controllers/sales_lines_controller.rb
@@ -20,7 +20,7 @@ class SalesLinesController < ApplicationController
       outlet_ttc_turnover = outlet.ttc_turnover += ttc_total
       farm_ht_turnover = @farm.ht_turnover += ht_total
       farm_ttc_turnover = @farm.ttc_turnover += ttc_total
-      sale.update(ht_total: ht_total, ttc_total: ttc_total)
+      sale.update(ht_total: sale_ht_total, ttc_total: sale_ttc_total)
       outlet.update(ht_turnover: outlet_ht_turnover, ttc_turnover: outlet_ttc_turnover)
       @farm.update(ht_turnover: farm_ht_turnover, ttc_turnover: farm_ttc_turnover)
       redirect_to farm_pointsdevente_sale_path(@farm, outlet, sale)
