87afcba4aa45aa5c89bdfdfc014fc33e79867633
Adelaide Blot <adelaide.blot@hotmail.fr>
2021-04-17 10:08:16 +0200

    flatpickr

---
+++ app/javascript/plugins/flatpickr.js
@@ -0,0 +1,25 @@
+import flatpickr from "flatpickr";
+import "flatpickr/dist/themes/material_green.css";
+import { French } from 'flatpickr/dist/l10n/fr.js';
+const initFlatPickr = () => {
+  flatpickr(".datepicker", {
+    locale: French,
+    altFormat: 'd M Y à H:i',
+    enableTime: true,
+    altInput: true,
+    mode: "multiple",
+  });
+
+  flatpickr(".timepicker", {
+    locale: French,
+    altFormat: 'd M Y',
+    altInput: true,
+    enableTime: true,
+    noCalendar: true,
+    dateFormat: "H:i",
+    time_24hr: true,
+    mode: "multiple",
+  });
+};
+
+export { initFlatPickr };
