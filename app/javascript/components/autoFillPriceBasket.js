f1c59d05c8c92d620c12e3bc92cce5f0f4196c8f
Adelaide Blot <adelaide.blot@hotmail.fr>
2021-04-20 23:42:04 +0200

    fix js paniers

--- app/javascript/components/autoFillPriceBasket.js
+++ app/javascript/components/autoFillPriceBasket.js
@@ -1,35 +1,23 @@
 const autoFillPriceBasket = () => {
-  // const array1 = [...Array(1000).keys()];
-  //   array1.forEach(element => {
-  //     if (document.querySelector(`#quantitejs${element}`)) {
-  //       const getQuantite = document.querySelector(`#quantitejs${element}`);
-  //       const getPTTTC = document.querySelector(`#prixtotaljs${element}`);
-  //       const getPUTTC = document.querySelector(`#prixunitairejs${element}`);
-  //       getQuantite.addEventListener('keyup', (event) => {
-  //         const totalTTC = Math.round(((parseFloat(getPUTTC.value) * parseFloat(getQuantite.value)) + Number.EPSILON) * 100) / 100;
-  //         getPTTTC.value = totalTTC;
-  //       });
+  const array1 = [...Array(1000).keys()];
+  array1.forEach(element => {
 
-  //       getPTTTC.addEventListener('keyup', (event) => {
-  //         const quantiteAAjouter = Math.round((parseFloat(getPTTTC.value) / parseFloat(getPUTTC.value)) * 100) / 100;
-  //         getQuantite.value = quantiteAAjouter;
-  //       });
-  //     };
-  //     if (document.querySelector(`#quantite${element}`)) {
-  //       const getQuantite = document.querySelector(`#quantite${element}`);
-  //       const getPTTTC = document.querySelector(`#prixtotal${element}`);
-  //       const getPUTTC = document.querySelector(`#prixunitaire${element}`);
-  //       getQuantite.addEventListener('keyup', (event) => {
-  //         const totalTTC = Math.round(((parseFloat(getPUTTC.value) * parseFloat(getQuantite.value)) + Number.EPSILON) * 100) / 100;
-  //         getPTTTC.value = totalTTC;
-  //       });
+    if (document.querySelector(`#basket_line_quantity${element}`)) {
+      const getQuantity = document.querySelector(`#basket_line_quantity${element}`);
+      const getPTTTC = document.querySelector(`#basket_line_ttc_total${element}`);
+      const getPUTTC = document.querySelector(`#basket_line_ttc_unit_price${element}`);
 
-  //       getPTTTC.addEventListener('keyup', (event) => {
-  //         const quantiteAAjouter = Math.round((parseFloat(getPTTTC.value) / parseFloat(getPUTTC.value)) * 100) / 100;
-  //         getQuantite.value = quantiteAAjouter;
-  //       });
-  //     };
-  //   });
+      getQuantity.addEventListener('keyup', (event) => {
+        const totalTTC = Math.round(((parseFloat(getPUTTC.value) * parseFloat(getQuantity.value)) + Number.EPSILON) * 100) / 100;
+        getPTTTC.value = totalTTC;
+      });
+
+      getPTTTC.addEventListener('keyup', (event) => {
+        const quantiteAAjouter = Math.round((parseFloat(getPTTTC.value) / parseFloat(getPUTTC.value)) * 100) / 100;
+        getQuantity.value = quantiteAAjouter;
+      });
+    };
+  });
 
 };
 
--- app/javascript/components/dateForCalendarEvent.js
+++ app/javascript/components/dateForCalendarEvent.js
@@ -3,14 +3,14 @@ const dateForCalendarEvent = () => {
   if (days) {
     days.forEach((day) => {
       day.addEventListener('dblclick', (event) => {
-        let date = event.currentTarget.dataset.date;
+        // let date = event.currentTarget.dataset.date;
 
         let modal = day.parentElement.lastElementChild;
         modal.classList.toggle("d-block");
         modal.classList.toggle("show");
 
         let startTime = modal.getElementsByClassName('start-time')[0]
-        startTime.value = date
+        // startTime.value = date
 
         let modalBtn = modal.firstElementChild.firstElementChild.firstElementChild.lastElementChild
         modalBtn.addEventListener('click', (event) => {
