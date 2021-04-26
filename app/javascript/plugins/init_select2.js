ad01b28fd9e85fe364e3c15d0f932f9dd1633a24
Adelaide Blot <adelaide.blot@hotmail.fr>
2021-04-26 21:22:53 +0200

    remove select2 sale show

--- app/javascript/plugins/init_select2.js
+++ app/javascript/plugins/init_select2.js
@@ -3,7 +3,7 @@ import 'select2';
 
 // function definitions
 const initSelect2 = () => {
-  $('#sales_line_product').select2({ placeholder: 'Produit' });
+  // $('#sales_line_product').select2({ placeholder: 'Produit' });
   // $('.basket-product-select').select2({ placeholder: 'Produit' });
   $('#beds_').select2({multiple: true});
 };
